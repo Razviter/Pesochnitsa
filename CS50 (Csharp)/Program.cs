d130e7e3f91ed19b8be5711cc2a53fe7442f413a
Павлушка <@MEGAKOMP>
2016-08-29 22:32:05 +0500

    Добавьте файлы проекта.

---
+++ CS50 (Csharp)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS50__Csharp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Фамилию Имя Отчество");
+            string name = Console.ReadLine();
+            Console.WriteLine(char.ToUpper(name[0]));
+            for (int i = 0, n = lentgh(name); i < n; i++)
+        }
+    }
+}
